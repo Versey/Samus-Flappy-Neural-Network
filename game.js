07e11e50d846cea135dcf8b45b2c3f76d9b037d6
Michael Leroux <mike_leroux121@hotmail.com>
2017-10-02 02:07:42 -0400

    Testing
    test

--- game.js
+++ game.js
@@ -56,27 +56,25 @@ var loadImages = function(sources, callback){
 
 var Samus = function(json){
 	// Position
-	this.x = 400;
-	this.y = 340;
+	this.x = 300;
+	this.y = 50;
 
 	// Scale
-	this.width = 88;
-	this.height = 46;
+	this.width = 46;
+	this.height = 88;
 
 	// Attributes
 	this.alive = true;
-	this.gravity = .6;
-	this.velocity = 15;
-	this.jumps = -6;
-	this.grounded = true;
-
+	this.gravity = 0;
+	this.velocity = .9;
+	this.jumps = -25;
+	this.grounded = false;
     this.shift = 80;
     this.frameWidth = 80;
     this.frameHeight = 86;
     this.totalFrames = 8;
     this.currentFrame = 1;
-
-    this.runspeed = 3;
+    this.runspeed = 2;
 
     this.faster = false;
     this.slower = false;
@@ -92,17 +90,19 @@ Samus.prototype.init = function(json){
 
 
 Samus.prototype.jump = function(){
-
-	if (this.grounded == true)
-    {
+    if(this.grounded == true) {
+    	this.y -= 3;
+        this.gravity = this.jumps;
         this.grounded = false;
+        //this.currentFrame = 1;
     }
+
 
 }
 
 Samus.prototype.quick = function(){
 
-	if(this.grounded == true && this.x < 1200) {
+	if(this.x < 1200) {
         this.faster = true;
         this.slower = false;
     }
@@ -111,7 +111,7 @@ Samus.prototype.quick = function(){
 
 Samus.prototype.slow = function(){
 
-    if(this.grounded == true && this.x > 100) {
+    if(this.x > 100) {
         this.faster = false;
         this.slower = true
     }
@@ -123,44 +123,26 @@ Samus.prototype.normal = function(){
 
 }
 
-Samus.prototype.update = function(){
-
-    this.y += this.gravity;
-
-    if (this.faster == true){
-    	this.x += this.runspeed;
-	}
-
-	else if (this.slower == true){
-    	this.x -= this.runspeed;
-	}
-
-    if(this.grounded == false){
-    	this.y -= this.velocity;
-    	this.velocity -= this.gravity;
-	}
-
-	if (this.y >= 340){
-		this.y = 340;
-		this.grounded = true;
-    }
+Samus.prototype.update = function() {
+	this.gravity += this.velocity;
+	this.y += this.gravity;
 
 
 }
 
 Samus.prototype.animate = function(context){
-
-
 		// Run
 	if (this.grounded) {
 
-        var limiter = 5;
+        var limiter = 3;
         context.drawImage(images.samusRun, this.shift, 0, this.frameWidth, this.frameHeight,
             this.x, this.y, this.frameWidth, this.frameHeight);
 
         if (this.faster == true){
             limiter = 3;
+
 		}
+
 		if (this.slower == true){
         	limiter = 6;
 		}
@@ -178,11 +160,13 @@ Samus.prototype.animate = function(context){
     }
 
     // In the air
-    else if (this.grounded == false ){
-        context.drawImage(images.samusRun, this.shift, 0, this.frameWidth, this.frameHeight,
-            this.x, this.y, this.frameWidth, this.frameHeight);
 
-        if (frame % 5 == 0) {
+    else if (this.grounded == false ){
+
+        context.drawImage(images.samusJump, this.shift, 0, this.frameWidth, this.frameHeight + 12,
+            this.x, this.y, this.frameWidth, this.frameHeight + 12);
+
+        if (frame % 3 == 0) {
 
             this.currentFrame++;
             this.shift += this.frameWidth;
@@ -202,13 +186,39 @@ Samus.prototype.animate = function(context){
 
 Samus.prototype.isGrounded = function(height, grounds){
 
-}
+    for(var i in grounds){
+        if(this.x < grounds[i].x + grounds[i].width &&
+            this.x + this.width > grounds[i].x &&
 
+            this.y <grounds[i].y + grounds[i].height &&
+            this.height + this.y > grounds[i].y)
+
+        {
+        	this.grounded = true;
+            return true;
+
+        }
+
+
+    }
+}
 Samus.prototype.isDead = function(height, platforms){
 	// TO DO
+
+	if (this.y > 512 || this.y < 0 - this.height){
+        return true;
+    }
+
+
+
 	for(var i in platforms){
-		if (this.x < platforms[i].x + platforms[i].width
-		&& this.x + this.width > platforms[i].x){
+        if(this.x < platforms[i].x + platforms[i].width &&
+            this.x + this.width > platforms[i].x &&
+
+            this.y <platforms[i].y + platforms[i].height &&
+            this.height + this.y > platforms[i].y)
+
+		{
 
 			console.log("dead");
 			return true;
@@ -227,7 +237,7 @@ var Ground = function(json){
 	this.x = 0;
 	this.y = 0;
 	this.width = 444;
-	this.height = 97;
+	this.height = 430;
 	this.speed = 5;
 
     this.init(json);
@@ -249,7 +259,6 @@ Ground.prototype.isOut = function(){
     }
 }
 
-
 ///////////////////////////////////////
 /////////////Platforms/////////////////
 ///////////////////////////////////////
@@ -257,9 +266,15 @@ Ground.prototype.isOut = function(){
 var Platform = function(json){
 	this.x = 0;
 	this.y = 0;
-	this.width = 189;
-	this.height = 158;
+	this.width = 70;
+	this.height = 70;
 	this.speed = 5;
+
+    this.shift = 70;
+    this.frameWidth = 70;
+    this.frameHeight = 70;
+    this.totalFrames = 6;
+    this.currentFrame = 1;
 
 	this.init(json);
 }
@@ -279,6 +294,24 @@ Platform.prototype.isOut = function(){
 	if(this.x + this.width < 0){
 		return true;
 	}
+}
+
+Platform.prototype.animate = function (context) {
+    context.drawImage(images.platform, this.shift, 0, this.frameWidth, this.frameHeight,
+        this.x, this.y, this.frameWidth, this.frameHeight);
+
+    if (frame % 6 == 0) {
+
+        this.currentFrame++;
+        this.shift += this.frameWidth;
+
+        if (this.currentFrame >= this.totalFrames) {
+            this.shift = 0;
+            this.currentFrame = 0;
+
+        }
+    }
+
 }
 
 ///////////////////////////////////////
@@ -305,11 +338,12 @@ var Game = function(){
 	this.gen = [];
 	this.alives = 0;
 	this.generation = 0;
-	this.backgroundSpeed = .3;
+	this.backgroundSpeed = .1;
 	this.backgroundx = 0;
 	this.maxScore = 0;
 	this.frame = 0;
 	this.begin = true;
+	this.gravity = 9;
 
 }
 
@@ -321,6 +355,7 @@ Game.prototype.start = function(){
 	this.samuss = [];
 
 	this.gen = Neuvol.nextGeneration();
+
 	for(var i in this.gen){
 		var b = new Samus();
 		this.samuss.push(b)
@@ -330,62 +365,80 @@ Game.prototype.start = function(){
 }
 
 Game.prototype.update = function(){
+
+
+
 	this.backgroundx += this.backgroundSpeed;
 	frame += 1;
 
-	//console.log("Current frame: " + this.frame);
+    var nextHoll = 0;
 
-	var nextHoll = 0;
+    if(this.samuss.length > 0){
 
-	if(this.samuss.length > 0){
-		for(var i = 0; i < this.platforms.length; i+=2){
-			if(this.platforms[i].x + this.platforms[i].width > this.samuss
-					[0].x){
-				nextHoll = this.platforms[i].height/this.height;
-				break;
-			}
-		}
+        for(var i = 0; i < this.platforms.length; i++){
 
-        for(var i = 0; i < this.grounds.length; i+=2){
-            if(this.grounds[i].x + this.grounds[i].width > this.samuss
-                    [0].x){
-                nextHoll = this.grounds[i].height/this.height;
+            if(this.platforms[i].x + this.platforms[i].width < this.samuss[0].x){
+                nextHoll = this.platforms[i].x - (this.samuss[i].x +this.samuss[i].width) ;
                 break;
             }
         }
-	}
+    }
 
 	for(var i in this.samuss){
 		if(this.samuss[i].alive){
 
 			var inputs = [
-			this.samuss[i].y / this.height,
+			this.samuss[i].x / this.width,
 			nextHoll
 			];
 
 			var res = this.gen[i].compute(inputs);
 
-			if(res > 0.46){
+			if(res > 0.6){
 				this.samuss[i].jump();
 			}
 
-            if(res > 0.44){
+            if(res > 0.4){
                 this.samuss[i].quick();
             }
-            if(res > 0.45){
+
+            if(res > 0.4){
                 this.samuss[i].slow();
             }
 
-            if(res > 0.455){
+            if(res > 0.5){
                 this.samuss[i].normal();
             }
 
-			this.samuss[i].update();
+            if(!this.samuss[i].isGrounded(this.samuss[i].height, this.grounds)){
+                this.samuss[i].update();
+
+                if (this.samuss[i].faster == true && this.samuss[i].x < this.width / 2){
+                    this.samuss[i].x += this.samuss[i].runspeed;
+				}
+
+                if (this.samuss[i].slower == true && this.samuss[i].x > 150){
+                    this.samuss[i].x -= this.samuss[i].runspeed;
+                }
+
+			}
+
+            if (this.samuss[i].faster == true && this.samuss[i].x < 1200) {
+                this.samuss[i].x += this.samuss[i].runspeed;
+            }
+
+            if (this.samuss[i].slower == true && this.samuss[i].x > 100){
+                this.samuss[i].x -= this.samuss[i].runspeed;
+            }
+
 
 			if(this.samuss[i].isDead(this.height, this.platforms)){
+
 				this.samuss[i].alive = false;
 				this.alives--;
-				//console.log(this.alives);
+
+				console.log(this.alives);
+
 				Neuvol.networkScore(this.gen[i], this.score);
 				if(this.isItEnd()){
 					this.start();
@@ -414,22 +467,31 @@ Game.prototype.update = function(){
     if(this.groundinterval == 0){
 		// Initial tiles at the start
 		if (this.begin) {
-            this.grounds.push(new Ground({x: 0, y: 430, height: 0}));
-            this.grounds.push(new Ground({x: 425, y: 430, height: 0}));
-            this.grounds.push(new Ground({x: 850, y: 430, height: 0}));
-            this.grounds.push(new Ground({x: 1275, y: 430, height: 0}));
-            this.grounds.push(new Ground({x: 1500, y: 430, height: 0}));
+
+            var randSpacing = Math.round((Math.random() * 1200)) +  500;
+
+            this.platforms.push(new Platform({x:randSpacing, y:366, height:this.height}));
+
+            this.grounds.push(new Ground({x: 0, y: 430}));
+            this.grounds.push(new Ground({x: 425, y: 430}));
+            this.grounds.push(new Ground({x: 850, y: 430}));
+            this.grounds.push(new Ground({x: 1275, y: 430}));
+            this.grounds.push(new Ground({x: 1500, y: 430}));
             this.begin = false;
         }
 
 
-        this.grounds.push(new Ground({x:this.width, y:430, height:0}));
+        this.grounds.push(new Ground({x:this.width, y:430}));
 
     }
 
     if(this.interval == 0){
-        this.platforms.push(new Platform({x:this.width, y:340, height:this.height}));
-        this.spawnInterval = Math.floor((Math.random() * 500) + 80);
+
+        var randInterval = Math.round((Math.random() * 300)) +  90;
+
+        this.platforms.push(new Platform({x:this.width, y:366, height:this.height}));
+        this.spawnInterval = randInterval ;
+
     }
 
     this.groundinterval++;
@@ -447,6 +509,7 @@ Game.prototype.update = function(){
 
 
 	var self = this;
+
 	if(FPS == 0){
 		setZeroTimeout(function(){
 			self.update();
@@ -465,20 +528,28 @@ Game.prototype.isItEnd = function(){
 			return false;
 		}
 	}
+
+	this.begin = true;
 	return true;
+}
+
+Game.prototype.scale = function (x,y){
+    this.ctx.scale(x,y);
 }
 
 Game.prototype.display = function(){
 
 	// Background Parallax //
-	this.ctx.clearRect(0, 0, this.width, this.height);
+
+
 	for(var i = 0; i < Math.ceil(this.width / images.background.width) + 1; i++){
 		this.ctx.drawImage(images.background, i * images.background.width - Math.floor(this.backgroundx%images.background.width), 0)
 	}
 
 	// Platforms //
 	for(var i in this.platforms){
-			this.ctx.drawImage(images.platform, this.platforms[i].x, this.platforms[i].y);
+			this.platforms[i].animate(this.ctx);
+
 
 	}
 
@@ -488,29 +559,23 @@ Game.prototype.display = function(){
 
     }
 
-	//this.ctx.fillStyle = "#FFC600";
-	//this.ctx.strokeStyle = "#CE9E00";
-
-
 	// Samus' //
 	for(var i in this.samuss){
+
 		if(this.samuss[i].alive){
 			this.ctx.save();
             this.samuss[i].animate(this.ctx);
-
-			this.ctx.translate(this.samuss[i].x + this.samuss[i].width/2, this.samuss[i].y + this.samuss[i].height/2);
-
-			//this.ctx.drawImage(images.samus, -this.samuss[i].width/2, -this.samuss[i].height/2, this.samuss[i].width, this.samuss[i].height);
+			//this.ctx.translate(this.samuss[i].x + this.samuss[i].width/4, this.samuss[i].y + this.samuss[i].height/3);
 			this.ctx.restore();
 		}
 	}
 
 	this.ctx.fillStyle = "white";
-	this.ctx.font="20px consolas";
-	this.ctx.fillText("Fitness : "+ this.score, 10, 75);
-	this.ctx.fillText("Max Fitness : "+this.maxScore, 10, 100);
-	this.ctx.fillText("Generation : "+this.generation, 10, 125);
-	this.ctx.fillText("Alive : "+this.alives+" / "+Neuvol.options.population, 10, 150);
+	this.ctx.font="14px consolas";
+	this.ctx.fillText("FITNESS: "+ this.score, 10, 15);
+	this.ctx.fillText("MAX FITNESS: "+this.maxScore, 10, 30);
+	this.ctx.fillText("GENERATION: "+this.generation, 10, 45);
+	this.ctx.fillText("ALIVE: "+this.alives+" / "+Neuvol.options.population, 10, 60);
 
 	var self = this;
 	requestAnimationFrame(function(){
@@ -520,22 +585,29 @@ Game.prototype.display = function(){
 
 window.onload = function(){
 	var sprites = {
-		samusRun:"./img/samusRun2.png",
-		background:"./img/parallax.png",
-		platform:"./img/platform.png",
-		ground: "./img/ground.png",
-		samusJump: "./img/samusRun2.png"
+		samusRun:"img/samusRun2.png",
+		background:"img/parallax.png",
+		platform:"img/temp.png",
+		ground: "img/ground.png",
+		samusJump: "img/samusJump.png"
 	}
 
 	var start = function(){
 		Neuvol = new Neuroevolution({
 			population:50,
-			network:[2, [2], 1],
+			network:[2, [10], 1],
+            elitism:0.2,            // Best networks kepts unchanged for the next
+            // generation (rate).
+            randomBehaviour:0.5,    // New random networks for the next generation
+            // (rate).
+            mutationRate:0.1,       // Mutation rate on the weights of synapses.
+            mutationRange:0.5,      // Interval of the mutation changes on the
 		});
 		game = new Game();
 		game.start();
 		game.update();
 		game.display();
+		//game.scale(.1,.1);
 	}
 
 
